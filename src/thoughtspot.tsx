bb7b6d8eae37a9b2849ae0cc54e484a551ec7a1e
Aparnaa-Marimuthu <aparnaa.marimuthu@7dxperts.com>
2026-01-22 15:19:10 +0530

    ▶ Initial UAE Thoughtspot embed app 1

---
+++ src/thoughtspot.tsx
@@ -0,0 +1,14 @@
+import { init, AuthType } from "@thoughtspot/visual-embed-sdk";
+
+export const initThoughtSpot = () => {
+  init({
+    thoughtSpotHost: import.meta.env.VITE_TS_HOST,
+    authType: AuthType.None,
+    getAuthToken: async () => {
+      const res = await fetch(import.meta.env.VITE_TS_AUTH_URL, {
+        credentials: "include",
+      });
+      return res.text();
+    },
+  });
+};
